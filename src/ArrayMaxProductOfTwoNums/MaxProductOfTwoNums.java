3e3c5aa616dca9f7c2473dc73dc154ba4650778f
David <destrada2193@gmail.com>
2021-10-18 14:27:50 -0700

    MaxProduct of Two Numbers Algorithm Complete

--- src/ArrayMaxProductOfTwoNums/MaxProductOfTwoNums.java
+++ src/ArrayMaxProductOfTwoNums/MaxProductOfTwoNums.java
@@ -1,19 +1,121 @@
 package ArrayMaxProductOfTwoNums;
 
+import java.util.Arrays;
+
 public class MaxProductOfTwoNums {
-	
+
 	// (1) Find the maximum product of two numbers in an unsorted array
 	// Take 1 input, an array of ints, and return the max product
 	// (2) Find the pair of indices instead of the max product.
-	// (3) 
-	
-	
-	
+
 	public static void main(String[] args) {
-		
-		int[] t1 = {5, 3, 2, 5, 7, 0, 1};
-		int[] 
-		
+
+		int[] t1 = { 5, 3, 2, 5, 7, 0, 1 };
+		int[] t2 = { -2, -1, -4, 5, 8, 0 };
+		int[] t3 = { -20, -10, 3, 9, -8 };
+
+		System.out.println(maxProduct(t1));
+		System.out.println(maxProduct(t2));
+		System.out.println(maxProduct(t3));
+
+		System.out.println();
+
+		System.out.println(maxProduct2(t1));
+		System.out.println(maxProduct2(t2));
+		System.out.println(maxProduct2(t3));
+
+		System.out.println();
+
+		System.out.println(maxProduct3(t1));
+		System.out.println(maxProduct3(t2));
+		System.out.println(maxProduct3(t3));
+
 	}
 
+	// Time complexity: O(n^2) (quadratic)
+	public static int maxProduct(int[] arr) {
+
+		int length = arr.length;
+		int max = Integer.MIN_VALUE;
+
+		if (length < 2) {
+			System.out.println("No max exists, returning single value");
+			return max;
+			// TODO: throw exception
+		}
+
+		for (int i = 0; i < length; i++) {
+			for (int j = i + 1; j < length; j++) { // for each element, iterate through rest of array
+				if (arr[i] * arr[j] > max) { // check if product is larger than our current "max" value
+					max = arr[i] * arr[j]; // if so, override max value with new product
+				}
+			}
+		} 
+		return max;
+	}
+	
+	// To make more efficient, we can pre-sort the array using Arrays.sort()
+	// Time complexity: < O(n^2)
+	public static int maxProduct2(int[] arr) {
+		
+		int length = arr.length;
+		
+		if (length < 2) {
+			System.out.println("No max value, returning single value");
+			return Integer.MIN_VALUE;
+		}
+		
+		Arrays.sort(arr);
+		
+		int maxProduct = arr[length - 2] * arr[length - 1]; // since its now sorted, multiply the last 2 indexes 
+		int minProduct = arr[0] * arr[1];
+		
+		if (maxProduct > minProduct) {
+			return maxProduct;
+		} else {
+			return minProduct;
+		}
+	}
+	
+	// Check to see if input is valid, then proceed.
+	// Find potential max / min values first, then find product 
+	// Time complexity: Linear O(n) --> OPTIMAL ! 
+	public static int maxProduct3(int[] arr) {
+		
+		int length = arr.length;
+		
+		if (length < 2) {
+			System.out.println("No max value, returning single value");
+			return Integer.MIN_VALUE;
+		}
+		
+		int max1 = arr[0];
+		int max2 = Integer.MIN_VALUE;
+		
+		int min1 = arr[0];
+		int min2 = Integer.MAX_VALUE;
+		
+		for (int i = 1; i < length; i++) {
+			if (arr[i] < min1) {
+				min2 = min1;
+				min1 = arr[i];
+			} else if (arr[i] < min2) {
+				min2 = arr[i];
+			} else if (arr[i] > max1) {
+				max2 = max1;
+				max1 = arr[i];
+			} else if (arr[i] > max2) {
+				max2 = arr[i];
+			}
+		}
+		
+		int maxProduct = max1 * max2;
+		int minProduct = min1 * min2;
+		
+		if (maxProduct > minProduct) {
+			return maxProduct;
+		} else {
+			return minProduct;
+		}
+	}
 }
